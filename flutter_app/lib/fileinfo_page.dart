8c27fc4a46efadf1dc3b5377f257a6a7064ab4cb
Administrator <admin@example.com>
2020-09-01 10:43:36 +0800

    提交文件详情页面

--- flutter_app/lib/fileinfo_page.dart
+++ flutter_app/lib/fileinfo_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'dart:ui';
 //import 'package:unirouter/unirouter.dart';
@@ -17,8 +19,20 @@ Color getCurColor()
   return  Colors.white;
 }
 
+_renameClick()
+{
+  //TODO 文件重命名功能
+  log('gsdfdsafg');
+}
+
+/*
+* 功能：文件详情列表项
+* title :文件信息项说明
+* name:显示的内容
+* bShowRName:是否展示重命名按钮
+* */
 @override
-Widget FileInfoWidget(String title,String name,bool bShowLink ,bool bShowRName) {
+Widget FileInfoWidget(String title,String name ,bool bShowRName) {
 
   final double bottonSize = 16;
   return new Container(
@@ -31,61 +45,46 @@ Widget FileInfoWidget(String title,String name,bool bShowLink ,bool bShowRName) 
     child: new Row(
      //mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        SizedBox(width: 16,),
-       Text(title,style: TextStyle(color:Color(0xff666666),fontSize: 14,decorationColor: Colors.white),),
+        Container(
+           width:70.0,
+          margin: EdgeInsets.only(left: 16),
+          child: new Text(title,
+            style: TextStyle(
+                color:Color(0xff666666),
+                fontSize: 14,
+                decorationColor: Colors.white,
+                fontWeight: FontWeight.w400),
+          ),
+        ),
+        //SizedBox(width: 16,),
+
         //new Expanded(child: null)
       Expanded(
         child: new Text(name,
           style: TextStyle(color: Color(0xff222222),
               fontSize: 14,
               decorationColor: Colors.white,
-              fontFamily:'PingFangSC-Medium'),
+              fontWeight: FontWeight.w600),
         )
       ),
 
        // new Expanded(child: null)
+        bShowRName ?
         new Container(
           width: 16,
           height: 16,
-          margin: EdgeInsets.only(right: 16),
-          color: Colors.red,
-        ),
+          margin: EdgeInsets.only(right: 20),
+          child: new FloatingActionButton(
+            onPressed: _renameClick,
+            child: new Image.asset('assets/images/op_tool_rename_black.png'),
+            backgroundColor: Colors.transparent,
+          )
+        ) : new Container( padding: EdgeInsets.all(0),)
       ],
 
       
     ),
   );
-  // return new Container(
-  //   height: 48,
-  //   //width: 414,
-  //   child: new Row(
-  //     children: <Widget>[
-  //       new Container(
-  //         padding: EdgeInsets.only(left: 16,top: 14.0,bottom: 14.0),
-  //         child: Text(title),
-  //       ),
-  //       new Container(
-  //         padding: EdgeInsets.only(right: bottonSize + 16 + 5),
-  //         child: Text(name),
-  //       ),
-  //     bShowRName ?
-  //     new Expanded(
-  //         child: new Container(
-  //           padding: EdgeInsets.only(right: 16.0),
-  //           child: new IconButton(icon: Image.asset('cctv.jpg'), onPressed: null),
-  //         ),
-  //     ) :  Container(
-  //       width: 0.0,
-  //       height: 0.0,
-  //     ),
-  //
-  //     bShowLink ? getSegmentLine() :  Container(
-  //       width: 0.0,
-  //       height: 0.0,
-  //     ),
-  //     ],
-  //   ),
-  // );
 }
 
 //分割线
@@ -98,13 +97,17 @@ getSegmentLine() {
     ),
   );
 }
-
+/*
+* 文件详情图片显示
+* assetName:显示图片的路径
+* */
 @override
-Widget CenterImage()
+Widget CenterImage(String assetName)
 {
   return Container(
     height: 120,
     width: 120,
+    //设置边框
     decoration: new BoxDecoration(
       border: new Border.all(width: 1.0,color: Color(0xffE3E3E3)), color: Color(0xffFAFAFA),
     ),
@@ -113,7 +116,7 @@ Widget CenterImage()
     child: new Center(
       child: new Container(
         padding: EdgeInsets.all(20),
-        child: new Image.asset('assets/images/cctv.jpg'),
+        child: new Image.asset(assetName.length > 0 ? assetName : 'assets/images/cctv.jpg'),
       ),
     ),
   );
@@ -135,18 +138,18 @@ class FileInfoPageState extends State<FileInfoPage> {
           width: 414,
           //color: Colors.white,
           padding: EdgeInsets.only(top: 40),
+          //设置背景颜色
           decoration: new BoxDecoration(
             //border: new Border.all(width: 1.0,color: Color(0xffE3E3E3)),
               color: getCurColor()
           ),
           child: new Column(
             children: <Widget>[
-              CenterImage(),
+              CenterImage(''),
             ],
           ),
         ),
       ],
-
     ),
   );
 
@@ -159,11 +162,17 @@ class FileInfoPageState extends State<FileInfoPage> {
 
         children: <Widget>[
           curview,
-          FileInfoWidget('文件命名：', 'jdsjgjk.txt', true, true),
+          FileInfoWidget('文件命名：', '好看的书.pdf', true),
           //添加下滑线
           getSegmentLine(),
-          FileInfoWidget('文件命名：', 'jdsjgjk.txt', true, true),
-          FileInfoWidget('文件命名：', 'jdsjgjk.txt', true, true),
+          FileInfoWidget('文件大小：', '335K', false),
+          getSegmentLine(),
+          FileInfoWidget('创建者：', '张三', false),
+          getSegmentLine(),
+          FileInfoWidget('创建时间：', '03-03 15.43', false),
+          FileInfoWidget('修改时间：', '03-03 15.43', false),
+          getSegmentLine(),
+          FileInfoWidget('文件路径：', '工作空间', false),
         ],
       ),
     );
